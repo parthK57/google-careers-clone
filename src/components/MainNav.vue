c517aa3328c10b1c9e01b5b446076207e4951210
parthK57 <parth.kolgiri32@gmail.com>
2023-04-10 18:32:25 +0530

    Directive - VFOR

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,6 +3,13 @@
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
         <a class="flex h-full items-center text-xl" :href="url">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li v-for="item in menuItems" :key="item" class="ml-9 h-full first:ml-0">
+              <a href="" class="flex h-full items-center py-2.5">{{ item }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -15,6 +22,14 @@ export default {
     return {
       company: "Google Careers",
       url: "https://careers.google.com",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Google Careers",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
